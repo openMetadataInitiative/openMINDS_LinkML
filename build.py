9fccad45fe1fbf827a71e190f99e42ef31469f26
Andrew Davison <andrew.davison@cnrs.fr>
2024-02-06 15:18:55 +0100

    Add top-level schema file

--- build.py
+++ build.py
@@ -1,6 +1,8 @@
 import os.path
 import shutil
 
+import yaml
+
 from pipeline.translator import LinkMLBuilder
 from pipeline.utils import clone_sources, SchemaLoader
 
@@ -15,10 +17,38 @@ if os.path.exists("target"):
     shutil.rmtree("target")
 
 for schema_version in schema_loader.get_schema_versions():
+    imports = []
 
     # Step 2 - find all involved schemas for the current version
     schemas_file_paths = schema_loader.find_schemas(schema_version)
 
     for schema_file_path in schemas_file_paths:
-        # Step 3 - translate and build each openMINDS schema as JSON-Schema
+        # Step 3 - translate and build each openMINDS schema as a LinkML schema
         LinkMLBuilder(schema_file_path, schema_loader.schemas_sources).build()
+        imports.append(
+            os.path.relpath(
+                schema_file_path, start=f"sources/schemas/{schema_version}"
+            ).replace(".schema.omi.json", "")
+        )
+
+    # Step 4 - create overall schema file
+    schema_metadata = {
+        "id": f"https://openminds.ebrains.eu/schemas/latest/?format=linkml",
+        "name": f'OpenMINDS version "{schema_version}"',
+        "description": f'The complete collection of schemas for all metadata models of the openMINDS metadata framework, version "{schema_version}"',
+        "license": "https://spdx.org/licenses/MIT.html",
+        "imports": ["linkml:types"] + imports,
+        "prefixes": {
+            "linkml": "https://w3id.org/linkml/",
+            "schema": "http://schema.org/",
+            "omi": "https://openminds.ebrains.eu",
+        },
+        "default_prefix": "omi",
+    }
+    with open(
+        os.path.join(
+            "target", "schemas", schema_version, f"openMINDS-{schema_version}.yml"
+        ),
+        "w",
+    ) as fp:
+        yaml.dump(schema_metadata, fp)
